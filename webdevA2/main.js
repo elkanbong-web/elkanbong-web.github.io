ebe2977beb36fc0a96ce5226d8f7da1fe8c96e21
elkanbong-web <elkanbong@gmail.com>
2026-07-31 18:36:27 +0800

    Update main.js

--- webdevA2/main.js
+++ webdevA2/main.js
@@ -49,14 +49,7 @@ function toggleMenus() {
     }
 }
 const bigword = document.querySelector(".bigword");
-function goHome() {
-    hideall();
 
-    bigword.style.display = "block";
-}
-const homeButton = document.querySelector("#home");
-
-homeButton.addEventListener("click", goHome);
 const lines = document.querySelectorAll(
     "#line1, #line2, #line3, #line4, #line5, #line6, #line7"
 );
